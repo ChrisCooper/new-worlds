7c42d952b2adcebf269b9f312e385d54bc83fa3c
Chris Cooper <chriscooper1991@gmail.com>
2013-03-12 10:31:15 -0400

    Made a simple framerate independant game loop

--- main.js
+++ main.js
@@ -1,53 +1,73 @@
 
-// PArameters
-var FPS_target = 30;
-var CANVAS_WIDTH = 640;
-var CANVAS_HEIGHT = 512;
+var x = 50;
+var y = 50;
 
-// Components
-var canvas;
 
-// State
-var textX = 50;
-var textY = 50;
+function Game() {
+    var self = this;
 
-function init() {
-  var canvasElement = $('<canvas width="' + CANVAS_WIDTH + '" height="' + CANVAS_HEIGHT + '"></canvas>');
-  canvas = canvasElement.get(0).getContext("2d");
+    self.FPS_target = 30;
+    
+    self.CANVAS_WIDTH = 640;
+    self.CANVAS_HEIGHT = 512;
 
-  canvasElement.appendTo('#canvasDiv');
+    // Components
+    self.canvas = null;
+
+
+    self.target_frame_interval = 1000/self.FPS_target;
+    
+
+    self.update = function(deltaT) {
+        //x = x + 1 * deltaT;
+        //y = y + 1 * deltaT;
+        console.log(deltaT);
+        x = x + 80 * deltaT;
+    }
+
+    self.draw = function() {
+        ctx = self.canvas;
+        ctx.clearRect(0, 0, self.CANVAS_WIDTH, self.CANVAS_HEIGHT);
+        ctx.fillStyle = "#000";
+        ctx.fillRect(x, y, 50, 50);
+    }
+
+    self.init = function() 
+    {
+        var canvasElement = $('<canvas width="' + self.CANVAS_WIDTH + '" height="' + self.CANVAS_HEIGHT + '"></canvas>');
+        self.canvas = canvasElement.get(0).getContext("2d");
+        
+        canvasElement.appendTo('#canvasDiv');
+    };
+
+
+    self.last_update_time = Date.now();
+
+    self.game_loop_step = function()
+    {
+        var deltaT = Date.now() - self.last_update_time;
+        self.last_update_time = Date.now();
+        
+        var frame_start_time = Date.now();
+        
+        ///////////////////////////////////
+        self.update(deltaT/1000);
+        self.draw();
+        ///////////////////////////////////
+        
+        var frame_run_time = Date.now() - frame_start_time;
+
+        var timeout_time = Math.max(self.target_frame_interval - frame_run_time, 0);
+
+        setTimeout(self.game_loop_step, timeout_time);
+    };
+
+    self.run = function() {
+        self.draw();
+        self.game_loop_step();
+    }
 }
 
-init();
-setInterval(function() {
-  update();
-  draw();
-}, 1000/FPS_target);
-
-
-function draw() {
-  canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  player.draw();
-}
-
-
-
-function update() {
-  textX += 1;
-  textY += 1;
-}
-
-
-
-var player = {
-  color: "#00A",
-  x: 220,
-  y: 270,
-  width: 32,
-  height: 32,
-
-  draw: function() {
-    canvas.fillStyle = this.color;
-    canvas.fillRect(this.x, this.y, this.width, this.height);
-  }
-};
+var game = new Game();
+game.init();
+game.run();
