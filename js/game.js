fdf192dd2b0ee9d1d73a689cde7f36f9fa981a4a
Chris Cooper <chriscooper1991@gmail.com>
2013-03-14 03:24:23 -0400

    Added simple randomly moving boxes

--- js/game.js
+++ js/game.js
@@ -1,20 +1,66 @@
-function Player() 
+function Player(initialX, initialY) 
 {    
     var self = this;
 
-    self.color = "#00A";
-    self.x = 220;
-    self.y = 270;
+    self.color = new Color(0, 0, 255, 0);
+    self.x = initialX;
+    self.y = initialY;
+    self.velX = 0;
+    self.velY = 0;
     self.width = 32;
     self.height = 32;
-
-    self.draw = function(canvas) {
-        canvas.fillStyle = self.color;
-        canvas.fillRect(self.x, self.y, self.width, self.height);
-    }
+    self.targetX = initialX;
+    self.targetY = initialY;
 
     self.update = function(deltaT) {
+        if (Math.random() < 0.1) {
+            var accX = (Math.random() - 0.5);
+            var accY = (Math.random() - 0.5);
 
+            self.velX += accX * deltaT * 50;
+            self.velY += accY * deltaT * 50;
+
+            
+        }
+
+        self.velX += (self.targetX - self.x) * deltaT * 0.01;
+        self.velY += (self.targetY - self.y) * deltaT * 0.01;
+
+        self.velX *= 1 - (0.1 * deltaT);
+        self.velY *= 1 - (0.1 * deltaT);
+
+        self.speed = Math.sqrt(self.velX*self.velX + self.velY*self.velY);
+
+        self.x += self.velX * deltaT * 50;
+        self.y += self.velY * deltaT * 50;
+    }
+
+    self.draw = function(canvas) {
+        canvas.fillStyle = self.color.redifiedRGBAstring(self.speed * 100);
+        canvas.fillRect(self.x, self.y, self.width, self.height);
+    }
+}
+
+function Color(r, g, b, a)
+{
+    this.red = r;
+    this.green = g;
+    this.blue = b;
+    this.aplha = b;
+
+    this.toRGBAstring = function() {
+        return this.RGBAstring(this.red, this.green, this.blue, this.aplha);
+    }
+
+    this.RGBAstring = function(r, g, b, a) {
+        return "rgba("+r+","+g+","+b+","+a+")";
+    }
+
+    this.redifiedRGBAstring = function(magnitude) {
+        magnitude = Math.floor(magnitude);
+        var r = Math.min(255, this.red + magnitude);
+        var b = Math.max(0, this.blue - magnitude);
+        return this.RGBAstring(r, this.green, b, this.aplha);
     }
 }
 
@@ -28,13 +74,16 @@ function addStats(game)
 }
 
 function Game() 
-{    var self = this;
+{    
+    var self = this;
 
     self.FPS_target = 60;
     self.minimum_FPS = 10;
     
-    self.CANVAS_WIDTH = 640;
-    self.CANVAS_HEIGHT = 512;
+    self.CANVAS_WIDTH = 1024;
+    self.CANVAS_HEIGHT = 640;
+
+    self.num_objects = 100;
 
     // Components
     self.canvas = null;
@@ -45,14 +94,14 @@ function Game()
     
 
     self.update = function(deltaT) {
-        self.player.x += 80 * deltaT;
+        self.players.map(function(p) {p.update(deltaT);});
     }
 
     self.draw = function() {
         ctx = self.canvas;
         ctx.clearRect(0, 0, self.CANVAS_WIDTH, self.CANVAS_HEIGHT);
 
-        self.player.draw(self.canvas);
+        self.players.map(function(p) {p.draw(self.canvas);});
     }
 
     self.init = function() 
@@ -62,7 +111,10 @@ function Game()
         
         canvasElement.appendTo('#canvasDiv');
 
-        self.player = new Player();
+        self.players = [];
+        for (var i = 0; i < self.num_objects; i++) {
+            self.players.push(new Player(self.CANVAS_WIDTH/2, self.CANVAS_HEIGHT/2));
+        }
 
         addStats(self);
     };
